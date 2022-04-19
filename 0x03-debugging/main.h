80f04ef6e0c52b2b4634e51c14da653ed846f662
leighsha <ashleighrufaro@outlook.com>
2022-04-20 00:03:07 +0200

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+​
+#include <stdio.h>
+​
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+​
+#endif
+vi 0-main.c, main.h
+[3~[3~[
+
+ls
+[3~[3~[3~[3~[3~[3[3[3[3[3[clear
+
+
+
+
+
+[3~[3[3~[3~[[3~[3[3[3[3[3[3[3[3[3[3[3[3[3[3
+
